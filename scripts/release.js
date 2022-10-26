5149a9ede6db0d507b1d6de6c74fa25b360fa386
Mazey/后除 <mazey@mazey.net>
2022-10-26 23:04:15 +0800

    release: v1.2.2

--- scripts/release.js
+++ scripts/release.js
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 // release
-require('mazey/scripts/release');
+const pkgVersion = process.env.VERSION || require('../package.json').version;
+const { release } = require('mazey/scripts/release');
+
+release(pkgVersion);
