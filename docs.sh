8e7b97a63a924f0284d02fb8ccb6d04ba09faa2c
Mazey Chu <mazey@mazey.net>
2023-08-31 00:02:19 +0800

    test: nvm 3

--- docs.sh
+++ docs.sh
@@ -11,6 +11,13 @@ git clone https://gitee.com/mirrors/nvm.git
 
 . nvm/install.sh
 
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm -v
+
+nvm list
+
 export NVM_NODEJS_ORG_MIRROR=https://registry.npmmirror.com/-/binary/node
 
 npm install
