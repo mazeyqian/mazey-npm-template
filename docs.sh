3fec57a5370d39e65441ef89680fe4d0a7add189
Mazey Chu <mazey@mazey.net>
2023-08-30 23:52:27 +0800

    test: nvm 0

---
+++ docs.sh
@@ -0,0 +1,16 @@
+node -v
+
+npm -v
+
+# nvm -v
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+# curl -o- https://gitee.com/weizy1983/nvm/raw/v0.39.1/install.sh | bash
+# wget -qO- https://gitee.com/weizy1983/nvm/raw/v0.39.1/install.sh | bash
+
+git clone git@gitee.com:mirrors/nvm.git
+
+. nvm/install.sh
+
+export NVM_NODEJS_ORG_MIRROR=https://registry.npmmirror.com/-/binary/node
+
+npm install --registry=http://registry.npm.bilibili.co
