b66c22fe162274474aa4d4228258b1c2c5228089
Mazey Chu <mazey@mazey.net>
2023-08-31 00:18:51 +0800

    test: nvm 6

--- docs.sh
+++ docs.sh
@@ -7,18 +7,29 @@ npm -v
 # curl -o- https://gitee.com/weizy1983/nvm/raw/v0.39.1/install.sh | bash
 # wget -qO- https://gitee.com/weizy1983/nvm/raw/v0.39.1/install.sh | bash
 
-git clone https://gitee.com/mirrors/nvm.git
+cd ~
 
-. nvm/install.sh
+git clone https://gitee.com/mirrors/nvm.git .nvm
 
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+cd ~/.nvm
+
+git checkout v0.39.5
+
+. ./nvm.sh
+
+# . nvm/install.sh
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm -v
 
-nvm ls
+nvm ls-remote
 
-export NVM_NODEJS_ORG_MIRROR=https://registry.npmmirror.com/-/binary/node
+export NVM_NODEJS_ORG_MIRROR=https://nodejs.org/dist
+
+nvm use system
 
 nvm install 14
 
@@ -26,4 +37,4 @@ nvm use 14
 
 node -v
 
-npm install
+# npm install
