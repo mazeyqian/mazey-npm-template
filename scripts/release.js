10f297aaa6d638ba68cdf06322e22f7986a09cb6
Mazey Chu <mazey@mazey.net>
2023-08-31 18:20:22 +0800

    chore(docs): remove unnecessary shell scripts

--- scripts/release.js
+++ scripts/release.js
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
-// release
 const { release } = require('mazey/scripts/git-helper.js');
 
 release(undefined, { defaultBranch: 'main' });
